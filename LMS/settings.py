aa3f28b5407c03feefb59bb4f2aaf95d3965a093
techtobit <nurthern2016@gmail.com>
2024-08-21 00:09:07 +0600

    debug false

--- LMS/settings.py
+++ LMS/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zb^wzdghe6wq4z06@v82v97e*o$io-d&ji!e300t9n5s01zm$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
